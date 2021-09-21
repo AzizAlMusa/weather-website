d7019f57d691805ae839ca539331088e4eb603b9
AzizAlmusa <abdulazizalmusa@gmail.com>
2021-09-21 10:35:33 +0300

    Added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,11 @@
 const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-  const url = "http://api.weatherstack.com/current?access_key=a75761b213dd0a3541840f8fb5f083cf&query=" + latitude + "," + longitude;
+  const url =
+    "http://api.weatherstack.com/current?access_key=a75761b213dd0a3541840f8fb5f083cf&query=" +
+    latitude +
+    "," +
+    longitude;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -9,7 +13,15 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.");
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degress out. The humidity is " +
+          body.current.humidity +
+          "%"
+      );
     }
   });
 };
